b9c401c5d7145e09c960e99d1256498ba3f9f1af
Pujon Das Auvi <auvipujondas@gmail.com>
2023-10-09 14:17:37 +0600

    folder structure done

---
+++ src/Index.js
@@ -0,0 +1,7 @@
+const app = require('./app')
+const config = require("./config/config")
+const port = config.app.port
+
+app.listen(port,async(req,res)=>{
+console.log(`Your server is running in http://localhost:${port}`)
+})
---
+++ src/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+require("./config/db");
+
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Home page
+app.get("/", (req, res) => {
+  res.status(200).sendFile(__dirname + "/./view/index.html");
+});
+
+//route not found error
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: "route not found",
+  });
+});
+
+//handling server error
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: "Something broke",
+  });
+});
+
+module.exports = app;
