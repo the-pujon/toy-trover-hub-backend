c9950099eb77e07d1be71bf7762e851a21285685
Pujon Das Auvi <auvipujondas@gmail.com>
2024-01-15 19:02:17 +0600

    update

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 require("./src/config/db");
-const userRouter = require("./src/routes/user.routes");
-const toyRouter = require("./src/routes/toy.routes");
+//const userRouter = require("./src/routes/user.routes");
+//const toyRouter = require("./src/routes/toy.routes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/api/users", userRouter);
-app.use("/api/toys", toyRouter);
+//app.use("/api/users", userRouter);
+//app.use("/api/toys", toyRouter);
 
 //Home page
 app.get("/", (req, res) => {
-  res.status(200).sendFile(__dirname + "/src/view/index.html");
-  //res.send({mes:"working"})
+  //res.status(200).sendFile(__dirname + "/src/view/index.html");
+  res.send({mes:"working"})
 });
 
 //route not found error
